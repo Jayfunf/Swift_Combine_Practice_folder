b76b82e129f452b6a58dffc818bcca4b8786c281
Jayfunf <simh3077@gmail.com>
2021-09-04 21:41:11 +0900

    modify: add_Comments

--- combine_passwordConfirm/combine_textfield_tutorial/MyViewModel.swift
+++ combine_passwordConfirm/combine_textfield_tutorial/MyViewModel.swift
@@ -3,26 +3,31 @@
 //  combine_textfield_tutorial
 //
 //  Created by Minhyun Cho on 2021/09/04.
-//
+//  Reference by 개발하는 정대리 on Youtube
+//  https://www.youtube.com/channel/UCutO2H_AVmWHbzvE92rpxjA
 
 import Foundation
 import Combine
 
 class MyViewModel { // 비밀번호 입력 시 이벤트를 받아줌.
     
-    @Published var passwordInput: String = "" {
-        didSet{
-            print("\(passwordInput)")
-        }
-    }
-    @Published var passwordConfirmInput: String = ""{
-        didSet {
-            print("\(passwordConfirmInput)")
-        }
-    }
+    // subscriber가 볼 수 있도록 @Published로 변수 선언. 게시자임.
+    @Published var passwordInput: String = "" //{
+//        didSet{
+//            print("\(passwordInput)")
+//        }
+//    }
+    @Published var passwordConfirmInput: String = "" //{
+//        didSet {
+//            print("\(passwordConfirmInput)")
+//        }
+//    }
     
     // 들어온 퍼블리셔들의 값 일치 여부를 반환 하는 퍼블리셔
     lazy var isMatchPasswordInput: AnyPublisher<Bool, Never> = Publishers
+        // lazy: 처음 사용 전 까진 로드하지 않음. 필요하지만 사용전 불러오기 부담스러운 친구들에게 사용.
+        // 예를 들어 페이스북의 알림같은 경우 알림 버튼을 사용자가 클릭하기 전까지 날라온 알림들을 로드해두지 않음. 메모리 낭비. 그럴 때 사용.
+        // 밑의 경우 passwordInput과 passwordConfirmInput이 들어오기 전까지 로드X
         .CombineLatest( $passwordInput, $passwordConfirmInput ) // 두 개시자로 부터 최신 요소를 받아 결합.
         .map({ (password: String, passwordConfirm: String) in
             if password == "" || passwordConfirm == "" {
--- combine_passwordConfirm/combine_textfield_tutorial/ViewController.swift
+++ combine_passwordConfirm/combine_textfield_tutorial/ViewController.swift
@@ -3,7 +3,8 @@
 //  combine_textfield_tutorial
 //
 //  Created by Minhyun Cho on 2021/09/04.
-//
+//  Reference by 개발하는 정대리 on Youtube
+//  https://www.youtube.com/channel/UCutO2H_AVmWHbzvE92rpxjA
 
 import UIKit
 import Combine
@@ -13,10 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var passwordConfirmTextfield: UITextField!
     @IBOutlet weak var myButton: UIButton!
+    // SwiftUI를 사용하지 않고 Storyboard 사용
     
     var viewModel: MyViewModel!
     
-    private var mySubscriptions = Set<AnyCancellable>() // 메모리 관리용 cancellable
+    private var mySubscriptions = Set<AnyCancellable>() // deinit될 때 자동으로 cancle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +29,15 @@ class ViewController: UIViewController {
         // 텍스트필드에서 나가는 이벤트를
         // 뷰모델의 프로퍼티가 구독
         passwordTextfield
-            .myTextPublisher
+            .myTextPublisher // extensiond으로 myTextPublisher기능 확장
 //            .print()
             // 스레드 - 메인에서 받겠다
+            // A publisher that delivers elements using the specified scheduler. - Apple Dev.
+            // 즉 on: DispatchQuqeu.main -> 디스패치큐 메인을 사용하여 요소를 전달.
             .receive(on: DispatchQueue.main)
             // 구독
-            .assign(to: \.passwordInput, on: viewModel)
-            .store(in: &mySubscriptions)
+            .assign(to: \.passwordInput, on: viewModel) // to: on에 있는, on: 어디에,to: 에는 <\.> <- 들어가야함.
+            .store(in: &mySubscriptions) // RxSwift의 disposeBag과 같이 사용 가능.
         
         passwordConfirmTextfield
             .myTextPublisher
@@ -42,7 +46,7 @@ class ViewController: UIViewController {
             .receive(on: RunLoop.main)
             // 구독
             .assign(to: \.passwordConfirmInput, on: viewModel)
-            .store(in: &mySubscriptions)
+            .store(in: &mySubscriptions) // RxSwift의 disposeBag과 같이 사용 가능.
         
         // 버튼이 뷰모델의 퍼블리셔를 구독
         viewModel.isMatchPasswordInput
@@ -50,7 +54,7 @@ class ViewController: UIViewController {
             .receive(on: RunLoop.main)
             // 구독
             .assign(to: \.isValid, on: myButton)
-            .store(in: &mySubscriptions)
+            .store(in: &mySubscriptions) // RxSwift의 disposeBag과 같이 사용 가능.
     }
 }
 
